e59743435c168a30db7a6306346b51a6f56f013f
Neil Edwards <Edwards@mac-pro.config>
2010-09-24 21:01:36 +0100

    initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Mmaa::Application.config.secret_token = '7cef4dda2d36e0e54e146893fd5bacb3513f3b3989f22206acd60c1991ac230bc0a6bd16055213895d4041d52c8ea7034e60f84776f5fe01832061d63246210e'
